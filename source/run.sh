b4427ac3ed7a372ad619b67aefbae68884435e4e
Rowan Shi <37481598+rowanxshi@users.noreply.github.com>
2019-01-23 14:31:37 -0500

    Update run.sh

--- source/run.sh
+++ source/run.sh
@@ -1,3 +1,3 @@
 Rscript ./r_master.r
 stata -b do analysis
-Rscript -e 'rmarkdown::render("../out/summary.Rmd", output_dir="../")'
+Rscript -e 'rmarkdown::render("../out/summary.Rmd", output_dir="../", output_format = "pdf_document")'
